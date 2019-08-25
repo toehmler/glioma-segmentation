9dbe423bb5462dabb5fc1c34ffd72572aeba1bc1
trey <treyoehmler@gmail.com>
2019-08-24 23:21:50 -0400

    change

--- test.py
+++ test.py
@@ -34,14 +34,16 @@ patient_scans = utils.norm_test_scans(patient_scans)
 test_slice = patient_scans[slice_no:slice_no+1,:,:,:4]
 test_label = patient_scans[slice_no:slice_no+1,:,:,4]
 
-
 prediction = model.predict(test_slice, verbose=1)
 preciction = np.argmax(prediction, axis=-1)
 print(prediction.shape)
 
-y = test_label[15:223,15:223]
+
+y = test_label[0,15:223,15:223]
 truth = y.reshape(43264,)
 print(classification_report(truth, prediction, labels=[0,1,2,3,4]))
+
+
 
 
 
