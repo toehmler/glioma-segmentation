169ff11dd5b80fb5e7818dd88bd57dde59a22b23
trey <treyoehmler@gmail.com>
2019-10-24 19:59:17 -0400

    try

--- models.py
+++ models.py
@@ -6,23 +6,24 @@ from keras.models import Model
 from keras import regularizers
 import keras.backend as K
 
-def f1_score(y_true, y_pred):
-    # Count positive samples.
-    c1 = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    c2 = K.sum(K.round(K.clip(y_true, 0, 1)))
-    c3 = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    # If there are no true samples, fix the F1 score at 0.
-    if c3 == 0:
-        return 0
-    # How many selected items are relevant?
-    precision = c1 / c2
-    # How many relevant items are selected?
-    recall = c1 / c3
-    # Calculate f1_score
-    f1_score = 2 * (precision * recall) / (precision + recall)
-    return f1_score
-
 def tri_path(input_shape):
+    def f1_score(y_true, y_pred):
+        # Count positive samples.
+        c1 = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        c2 = K.sum(K.round(K.clip(y_true, 0, 1)))
+        c3 = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        # If there are no true samples, fix the F1 score at 0.
+        if c3 == 0:
+            return 0
+        # How many selected items are relevant?
+        precision = c1 / c2
+        # How many relevant items are selected?
+        recall = c1 / c3
+        # Calculate f1_score
+        f1_score = 2 * (precision * recall) / (precision + recall)
+        return f1_score
+
+
     X_input = Input(input_shape)
 
     local = Conv2D(64, (4,4),
