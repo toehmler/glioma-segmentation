11f3144fadfd606c0a2b389b07a105d48036c2e9
trey <treyoehmler@gmail.com>
2019-10-24 19:54:26 -0400

    chamged from acc to f1 metric

--- models.py
+++ models.py
@@ -4,6 +4,23 @@ from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooli
 from keras.layers import Maximum
 from keras.models import Model
 from keras import regularizers
+import keras.backend as K
+
+def f1_score(y_true, y_pred):
+    # Count positive samples.
+    c1 = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    c2 = K.sum(K.round(K.clip(y_true, 0, 1)))
+    c3 = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    # If there are no true samples, fix the F1 score at 0.
+    if c3 == 0:
+        return 0
+    # How many selected items are relevant?
+    precision = c1 / c2
+    # How many relevant items are selected?
+    recall = c1 / c3
+    # Calculate f1_score
+    f1_score = 2 * (precision * recall) / (precision + recall)
+    return f1_score
 
 def tri_path(input_shape):
     X_input = Input(input_shape)
@@ -37,7 +54,7 @@ def tri_path(input_shape):
     out = Activation('softmax')(out)
 
     model = Model(inputs=X_input, outputs=out)
-    model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+    model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=[f1_score])
     return model
 
 def two_path(input_shape):
--- test.py
+++ test.py
@@ -18,14 +18,14 @@ loaded_model_json = json_file.read()
 json_file.close()
 
 model = model_from_json(loaded_model_json)
-model.load_weights("outputs/models/tri_path_08_train.h5")
+model.load_weights("outputs/models/tri_path_09_train.h5")
 #print(model.summary())
 
 with open('config.json') as config_file:
     config = json.load(config_file)
 root = config['root']
 
-patient_no = 201
+patient_no = 202 
 slice_no = 70
 
 
