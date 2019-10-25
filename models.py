dd7c04971721b4338632c4d751efd43ea2313d78
trey <treyoehmler@gmail.com>
2019-10-24 20:01:44 -0400

    try

--- models.py
+++ models.py
@@ -7,23 +7,6 @@ from keras import regularizers
 import keras.backend as K
 
 def tri_path(input_shape):
-    def f1_score(y_true, y_pred):
-        # Count positive samples.
-        c1 = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        c2 = K.sum(K.round(K.clip(y_true, 0, 1)))
-        c3 = K.sum(K.round(K.clip(y_pred, 0, 1)))
-        # If there are no true samples, fix the F1 score at 0.
-        if c3 == 0:
-            return 0
-        # How many selected items are relevant?
-        precision = c1 / c2
-        # How many relevant items are selected?
-        recall = c1 / c3
-        # Calculate f1_score
-        f1_score = 2 * (precision * recall) / (precision + recall)
-        return f1_score
-
-
     X_input = Input(input_shape)
 
     local = Conv2D(64, (4,4),
@@ -92,6 +75,24 @@ def two_path(input_shape):
     return model
 
 if __name__ == "__main__":
+    def f1_score(y_true, y_pred):
+        # Count positive samples.
+        c1 = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        c2 = K.sum(K.round(K.clip(y_true, 0, 1)))
+        c3 = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        # If there are no true samples, fix the F1 score at 0.
+        if c3 == 0:
+            return 0
+        # How many selected items are relevant?
+        precision = c1 / c2
+        # How many relevant items are selected?
+        recall = c1 / c3
+        # Calculate f1_score
+        f1_score = 2 * (precision * recall) / (precision + recall)
+        return f1_score
+
+
+
 
     name = input('Model name: ')
     train_model = tri_path((33,33,4))
