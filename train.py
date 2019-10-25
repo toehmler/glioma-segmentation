e2461c187ee4156aaa32643524532f4ac713c398
Ubuntu <bchode@chode.3dw0s0an5vxebiswiq2bxgwl3a.bx.internal.cloudapp.net>
2019-10-25 14:47:14 +0000

    testing whole brain tri_path_18

--- train.py
+++ train.py
@@ -86,7 +86,7 @@ if __name__ == '__main__':
 
         print('Slice no {}'.format(slice_no))
         # changed batch size to 256 from 1024 
-        model.fit(x,y,epochs=5,batch_size=512,class_weight=class_weights, validation_split=0.4)
+        model.fit(x,y,epochs=5,batch_size=1024,class_weight=class_weights, validation_split=0.4)
         model.save('outputs/models/{}_train.h5'.format(model_name))
         model.save_weights('{}_train_weights.h5'.format(model_name))
 
