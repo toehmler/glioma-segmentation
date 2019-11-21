e4bb05bcbfd54151ec8c010eea2e1d66b84e77e0
trey <treyoehmler@gmail.com>
2019-11-21 14:09:28 -0500

    changed testing script

--- test.py
+++ test.py
@@ -62,6 +62,7 @@ if __name__ == '__main__':
         truth = test_label[0,15:223,15:223]
 #        truth = truth.reshape(43264,)
 #        prediction = prediction.reshape(43264,)
+        '''
 
         scan = test_slice[0,:,:,2]
         tmp_label = test_label[0]
@@ -80,6 +81,7 @@ if __name__ == '__main__':
         plt.title('Prediction')
         plt.imshow(tmp_pred,cmap='gray')
         plt.savefig('outputs/{}_pat{}_slice{}.png'.format(model_name,patient_no,slice_no), bbox_inches='tight')
+        '''
 
         gt.extend(truth)
         pred.extend(prediction)
